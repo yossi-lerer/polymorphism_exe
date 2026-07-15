f0e042ac4acbd9ad0ea106231281f277d1be4bc7
yossi-lerer <yossi.lerer.2003@gmail.com>
2026-07-15 14:26:40 +0300

    finish the step 2 - Deactivate with Override

--- main.py
+++ main.py
@@ -21,3 +21,23 @@ lg = SmartTV("LG")
 google = SmartSpeaker("google")
 lg.activate()
 google.activate()
+# step 2 - Deactivate with Override
+class Device:
+    def __init__(self, name):
+        self.name = name
+    def deactivate(self):
+        print(f"Device {self.name} is now off.")
+class SmartLamp(Device):
+    def __init__(self, name):
+        super().__init__(name)
+    def deactivate(self):
+        print(f"Lamp {self.name} is dimming and turning off.")
+class SmartAC(Device):
+    def __init__(self, name):
+        super().__init__(name)
+    def deactivate(self):
+        print(f"AC {self.name} is cooling down and switching off.")
+bedroom_lamp = SmartLamp("Bedroom Lamp")
+living_room_AC =  SmartAC("Living Room AC")
+bedroom_lamp.deactivate()
+living_room_AC.deactivate()
