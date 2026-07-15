245cdca1636be05ae81ed7d3a5f56474b90171a3
yossi-lerer <yossi.lerer.2003@gmail.com>
2026-07-15 14:34:47 +0300

    finish the step 3 - Status Reports

--- main.py
+++ main.py
@@ -41,3 +41,31 @@ bedroom_lamp = SmartLamp("Bedroom Lamp")
 living_room_AC =  SmartAC("Living Room AC")
 bedroom_lamp.deactivate()
 living_room_AC.deactivate()
+# step 3 - Status Reports
+class Device:
+    def __init__(self, name, is_on):
+        self.name = name
+        self.is_on = is_on
+    def status(self):
+        if self.is_on == True:
+            print(f"{self.name}: on")
+        else:
+            print(f"{self.name}: off")
+            
+class SmartTV(Device):
+    def __init__(self, name, is_on, channel):
+        super().__init__(name, is_on)
+        self.channel = channel
+    def status(self):
+        if self.is_on == True:
+            print(f"{self.name}: on, watching channel {self.channel}")
+        else:
+            print(f"{self.name}: off")
+class SmartSpeaker(Device):
+    def __init__(self, name, is_on, song):
+        super().__init__(name, is_on)
+        self.song = song
+lg = SmartTV("LG", True, 8)
+alexa =  SmartSpeaker("Alexa", True, "Bohemian Rhapsody")
+lg.status()
+alexa.status()
